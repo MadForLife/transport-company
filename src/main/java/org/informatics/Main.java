3d2e0b7f5a9978785ad3c1e332d94ba1276fbf4a
MadForLife <madforlifee@gmail.com>
2024-01-17 18:11:05 +0200

    Revert "Implement DAO Interfaces for Entity Classes"
    This reverts commit 46eca85578cd58cf79687c099564b2b28a326c64.

--- src/main/java/org/informatics/Main.java
+++ src/main/java/org/informatics/Main.java
@@ -2,30 +2,10 @@ package org.informatics;
 
 import org.hibernate.SessionFactory;
 import org.informatics.configuration.SessionFactoryUtil;
-import org.informatics.dao.*;
-import org.informatics.dao.impl.*;
-import org.informatics.entity.*;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
         SessionFactory sessionFactory = SessionFactoryUtil.getSessionFactory();
-
-        ClientDao clientDao = new ClientDaoImpl(sessionFactory);
-        EmployeeDao employeeDao = new EmployeeDaoImpl(sessionFactory);
-        PersonDao personDao = new PersonDaoImpl(sessionFactory);
-        QualificationDao qualificationDao = new QualificationDaoImpl(sessionFactory);
-        TransportClientDao transportClientDao = new TransportClientDaoImpl(sessionFactory);
-        TransportCompanyDao transportCompanyDao = new TransportCompanyDaoImpl(sessionFactory);
-        TransportDao transportDao = new TransportDaoImpl(sessionFactory);
-        TransportTypeDao transportTypeDao = new TransportTypeDaoImpl(sessionFactory);
-        VehicleDao vehicleDao = new VehicleDaoImpl(sessionFactory);
-        VehicleTypeDao vehicleTypeDao = new VehicleTypeDaoImpl(sessionFactory);
-
-
     }
 }
